32a8693fb3314b975a9ce802ece20b6a18d38fe9
win-pc <wang5620079@163.com>
2019-02-20 23:15:13 +0800

    实现循环展现路由

--- src/App.vue
+++ src/App.vue
@@ -4,72 +4,66 @@
       <div class='row'>
         <div class='col-lg-2' style="background-color: gray">
           <div class="panel-group" id="accordion">
-            <div class="panel panel-default">
+            <div class="panel panel-default" v-for="item in items" :key="item.tabid">
               <div class="panel-heading">
                 <h4 class="panel-title">
                   <a data-toggle="collapse" data-parent="#accordion"
-                     href="#collapseOne">
-                    点击我进行展开，再次点击我进行折叠。第 1 部分
+                     :href = "['#'+item.tabid]">
+                    {{item.name}}
                   </a>
                 </h4>
               </div>
-              <div id="collapseOne" class="panel-collapse collapse in">
+              <div v-bind:id=item.tabid class="panel-collapse collapse in" v-if="item.tabid==='servicectrl'">
                 <div class="panel-body">
-                  Nihil anim keffiyeh helvetica, craft beer labore wes anderson
-                  cred nesciunt sapiente ea proident. Ad vegan excepteur butcher
-                  vice lomo.
+                  <router-link :to="{ path: item.path}">测试</router-link>
                 </div>
               </div>
-            </div>
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <h4 class="panel-title">
-                  <a data-toggle="collapse" data-parent="#accordion"
-                     href="#collapseTwo">
-                    点击我进行展开，再次点击我进行折叠。第 2 部分
-                  </a>
-                </h4>
-              </div>
-              <div id="collapseTwo" class="panel-collapse collapse">
+              <div v-bind:id=item.tabid class="panel-collapse collapse" v-else>
                 <div class="panel-body">
-                  Nihil anim keffiyeh helvetica, craft beer labore wes anderson
-                  cred nesciunt sapiente ea proident. Ad vegan excepteur butcher
-                  vice lomo.
-                </div>
-              </div>
-            </div>
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <h4 class="panel-title">
-                  <a data-toggle="collapse" data-parent="#accordion"
-                     href="#collapseThree">
-                    点击我进行展开，再次点击我进行折叠。第 3 部分
-                  </a>
-                </h4>
-              </div>
-              <div id="collapseThree" class="panel-collapse collapse">
-                <div class="panel-body">
-                  Nihil anim keffiyeh helvetica, craft beer labore wes anderson
-                  cred nesciunt sapiente ea proident. Ad vegan excepteur butcher
-                  vice lomo.
+                  {{item.description}}
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class='col-lg-10' style="background-color: red"><router-view></router-view></div>
+        <div class='col-lg-10'><router-view></router-view></div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'app'
+  name: 'app',
+  data: function () {
+    return {
+      items: [
+        {
+          name: '服务控制',
+          description: '服务控制，包含服务的启停',
+          tabid: 'servicectrl',
+          items: [{name: 'weblogic启动', divid: 'start_weblogic_divid'}, {
+            name: 'weblogic停止',
+            divid: 'stop_weblogic_divid'
+          }],
+          path: '/servicectrlcmp'
+        },
+        {
+          name: '服务监控',
+          description: '服务监控，包含参数获取',
+          tabid: 'servicemonitor',
+          items: [{name: 'weblogic监控', divid: 'weblogic_monitor'}, {name: 'weblogic数据源', divid: 'weblogic_datasorce'}]
+        },
+        {
+          name: '其他',
+          tabid: 'others'
+        }
+      ]
+    }
+  }
 }
 </script>
-
-
 
 <style>
   #app {
---
+++ src/components/ServiceCtrlCmp.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>服务启停</div>
+</template>
+
+<script>
+export default {
+  name: 'ServiceCtrlCmp'
+}
+</script>
+
+<style scoped>
+
+</style>
